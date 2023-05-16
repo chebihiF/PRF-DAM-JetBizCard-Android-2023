106fac83be1469fc9f5f3da43e4f013f095c64f7
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-05-16 14:53:56 +0100

    final version

--- app/src/main/java/org/m2i/jetbizcard/MainActivity.kt
+++ app/src/main/java/org/m2i/jetbizcard/MainActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -30,11 +32,15 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.m2i.jetbizcard.ui.theme.JetBizCardTheme
@@ -56,6 +62,11 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CreateBizCard(){
+
+    val buttonClickedState = remember {
+        mutableStateOf(true)
+    }
+
     Surface(modifier = Modifier.fillMaxSize()) {
         Card(modifier = Modifier
             .width(200.dp)
@@ -75,11 +86,19 @@ fun CreateBizCard(){
                 Divider()
                 CreateInfo()
                 Button(onClick = {
-                    Log.d("Clicked","Create BizCard")
+                    buttonClickedState.value = !buttonClickedState.value
+
                 }) {
                     Text(
                         text = "Portfolio",
                         style = MaterialTheme.typography.labelLarge)
+                }
+                if(buttonClickedState.value){
+                    content()
+                }else{
+                    Box(){
+
+                    }
                 }
             }
 
@@ -107,9 +126,32 @@ fun content() {
 
 @Composable
 fun Portfolio(data: List<String>) {
-    LazyColumn{
-        items(data){
-            item -> Text(text = item)
+    LazyColumn(modifier = Modifier.background(MaterialTheme.colorScheme.surface)){
+        items(data){ item ->
+            Card(modifier = Modifier
+                .padding(13.dp)
+                .fillMaxWidth(),
+                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.surface,
+                ),
+                shape = RectangleShape) {
+                Row(modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.surface)
+                    ) {
+                    CreateImageProfile(modifier = Modifier
+                        .size(100.dp)
+                        .padding(8.dp))
+                    Column(modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.CenterVertically)) {
+                        Text(text = item, fontWeight = FontWeight.Bold)
+                        Text(text = "A great project", style = MaterialTheme.typography.labelMedium)
+                    }
+                }
+
+            }
         }
     }
 }
@@ -135,15 +177,15 @@ private fun CreateInfo() {
 }
 
 @Composable
-private fun CreateImageProfile() {
+private fun CreateImageProfile(modifier: Modifier = Modifier) {
     Surface(
-        modifier = Modifier
+        modifier = modifier
             .size(150.dp)
             .padding(5.dp),
         shape = CircleShape,
         color = Color.White.copy(alpha = 0.5f),
         tonalElevation = 8.dp,
-        shadowElevation = 4.dp,
+        shadowElevation = 1.dp,
         border = BorderStroke(0.5.dp, Color.LightGray),
     ) {
         Image(
@@ -156,7 +198,7 @@ private fun CreateImageProfile() {
 }
 
 
-//@Preview(showBackground = true)
+@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     JetBizCardTheme {
